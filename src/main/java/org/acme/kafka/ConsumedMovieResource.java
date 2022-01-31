ac76ce5a9301105d492bbc183216bb02c60b13c7
Mike Hepburn <eformat@gmail.com>
2022-02-01 09:45:59 +1000

    🐙 initial commit 🐙

---
+++ src/main/java/org/acme/kafka/ConsumedMovieResource.java
@@ -0,0 +1,27 @@
+package org.acme.kafka;
+
+import io.smallrye.mutiny.Multi;
+import org.acme.kafka.quarkus.Movie;
+import org.eclipse.microprofile.reactive.messaging.Channel;
+import org.jboss.resteasy.reactive.RestSseElementType;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@ApplicationScoped
+@Path("/consumed-movies")
+public class ConsumedMovieResource {
+
+    @Channel("movies-from-kafka")
+    Multi<Movie> movies;
+
+    @GET
+    @Produces(MediaType.SERVER_SENT_EVENTS)
+    @RestSseElementType(MediaType.TEXT_PLAIN)
+    public Multi<String> stream() {
+        return movies.map(movie -> String.format("'%s' from %s", movie.getTitle(), movie.getYear()));
+    }
+}
